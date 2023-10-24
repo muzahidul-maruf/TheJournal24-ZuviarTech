460396a22b2125580d573c482034a3b6e84575e5
muzahidul-maruf <marufmuzahidul@gmail.com>
2023-10-24 15:26:30 +0600

    home page done

--- resources/views/frontend/_partials/header.blade.php
+++ resources/views/frontend/_partials/header.blade.php
@@ -1,4 +1,4 @@
-<div class="main-navbar clearfix bg-dark ">
+<div class="main-navbar clearfix" style="background-color: #013545">
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
@@ -19,29 +19,14 @@
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
                                     aria-haspopup="true" aria-expanded="false">
-                                    Home
+                                    বাংলাদেশ
                                 </a>
                                 <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="index.html">Home 1</a>
-                                    <a class="dropdown-item" href="index-2.html">Home 2</a>
+                                    <a class="dropdown-item" href="index.html">বাংলাদেশ 1</a>
+                                    <a class="dropdown-item" href="index-2.html">বাংলাদেশ 2</a>
                                 </div>
                             </li>
-
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
-                                    aria-haspopup="true" aria-expanded="false">
-                                    Post
-                                </a>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="post-left-sidebar.html">Post Left Sidebar</a>
-                                    <a class="dropdown-item" href="post-full-width.html">Post Full Width</a>
-                                    <a class="dropdown-item" href="single-post.html">Single Post</a>
-                                    <a class="dropdown-item" href="post-category-1.html">Category 1</a>
-                                    <a class="dropdown-item" href="post-category-2.html">Category 2</a>
-                                    <a class="dropdown-item" href="author.html">Author</a>
-                                </div>
-                            </li>
-                            <li class="nav-item dropdown">
+                            {{-- <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
                                     aria-haspopup="true" aria-expanded="false">
                                     Account
@@ -50,33 +35,28 @@
                                     <a class="dropdown-item" href="account.html">Log In</a>
                                     <a class="dropdown-item" href="signup.html">Register</a>
                                 </div>
-                            </li>
-
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
-                                    aria-haspopup="true" aria-expanded="false">
-                                    About
-                                </a>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="about.html">About</a>
-                                    <a class="dropdown-item" href="terms.html">Terms</a>
-                                    <a class="dropdown-item" href="privacy.html">Privacy Policy</a>
-                                    <a class="dropdown-item" href="job.html">Career</a>
-                                </div>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
-                                    aria-haspopup="true" aria-expanded="false">
-                                    Pages
-                                </a>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="404.html">404 Page</a>
-                                    <a class="dropdown-item" href="search.html">Search Page</a>
-                                </div>
-                            </li>
+                            </li> --}}
 
                             <li class="nav-item">
-                                <a class="nav-link" href="contact.html">Contact</a>
+                                <a class="nav-link" href="#">আন্তর্জাতিক</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">খেলাধুলা</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">বিনোদন</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">ফিচার</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">দেশজুড়ে</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">মতামত</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">অন্যান্য</a>
                             </li>
 
                         </ul>
@@ -92,8 +72,7 @@
         </div>
     </div>
     <form class="site-search" method="get">
-        <input type="text" id="searchInput" name="site_search" placeholder="Enter Keyword Here..."
-            autofocus="">
+        <input type="text" id="searchInput" name="site_search" placeholder="Enter Keyword Here..." autofocus="">
         <div class="search-close">
             <span class="close-search">
                 <i class="fa fa-times"></i>
@@ -101,5 +80,3 @@
         </div>
     </form>
 </div>
-
-<div class="py-30"></div>
